2913a25d5fd2e4632231416b0b04b68c4fe13de3
Basu macbookpro <basu@shrigowri.com>
2017-05-15 12:04:24 +0530

    initial commit

---
+++ Talkative Parents/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Talkative Parents
+//
+//  Created by Basavaraj Kalaghatagi on 15/05/17.
+//  Copyright © 2017 SGS. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Talkative Parents/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Talkative Parents
+//
+//  Created by Basavaraj Kalaghatagi on 15/05/17.
+//  Copyright © 2017 SGS. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
